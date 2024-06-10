722cb0412fa4d7b5ead5d516eac76b7200693c68
MarwaBaniFadil <marwa.bassam.2002@gmail.com>
2024-06-10 22:57:03 +0300

    after deleting the duplication btw marwa&batool

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,17 +1,13 @@
 const express=require("express");
 const router=express.Router();
 <<<<<<< HEAD
-const crops = require('../controllers/crops'); 
+ 
 const knowledgebase = require('../controllers/knowledgebase'); 
 
-router.get('/showallcrops',crops.showallcrops);
+
 router.get('/showallknowledgebase' ,knowledgebase.showallknowledgebase);
 router.get('/search_knowledgebase_by_author/:id',knowledgebase.search_knowledgebase_by_author_id);
 router.get('/search_knowledgebase_by_title/:title',knowledgebase.search_knowledgebase_by_title);
-
-router.delete('/deleteIDcrop/:id',crops.deleteIDcrop);
-router.delete('/deleteCropByUserId/:id',crops.deleteCropByUserId);
-router.delete('/deleteCropBygardenId/:id',crops.deleteCropBygardenId);
 router.delete('/delete_knowledgebase_by_guide_id/:id',knowledgebase.delete_knowledgebase_by_guide_id);
 router.delete('/delete_knowledgebase_by_author_id/:id',knowledgebase.delete_knowledgebase_by_author_id);
 
